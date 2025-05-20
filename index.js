82d7545154c94623d32aa473c9c15e987857ded9
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-20 18:33:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,8 @@
-// const fs = require('fs');
-const fsPromises = require('fs').promises;
-const path = require('path');
+// console.log('testing!')
 
-const fileOps = async () => {
-    try {
-        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
-        console.log(data);
-        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'));
-        await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
-        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet you');
-        await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
+const {format} = require('date-fns');
+const {v4 : uuid} = require('uuid');
 
-        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
-        console.log(newData);
-        // console.log('File Deleted');
-    } catch (err) {
-        console.error(err);
-    }
-}
-fileOps();
+console.log(format(new Date(), 'yyyy-MM-dd\tHH:mm:ss'));
 
-// fs.readFile(path.join(__dirname, 'files', 'starter.txt') , 'utf8', (err, data) => {
-//     if (err) throw err;
-//     // console.log(data.toString());
-//     console.log(data);
-// })
-
-// console.log("Hello...........")
-
-// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you', (err) => {
-//     if (err) throw err;
-//     // console.log(data.toString());
-//     console.log('Write Complete');
-
-//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n \nYes it is', (err) => {
-//         if (err) throw err;
-//     // console.log(data.toString());
-//     console.log('Append Complete');
-
-//     fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
-//         if (err) throw err;
-//     // console.log(data.toString());
-//     console.log('Rename Complete');
-//     })
-// });
-
-
-// process.on('uncaughtException', (err) => {
-//     console.error('There was an uncaught error', err);
-//     process.exit(1); // Exit process after error
-// })});
+console.log(uuid());
