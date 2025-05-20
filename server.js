8cb257c448e165ae0154be0212916f7306f48384
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-20 16:08:21 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,17 +7,23 @@ console.log("Hello World!")
 //(5) CommonJS modules instead of ES6 modules.
 const os = require('os');
 const path = require('path');
+const { add, subtract, multiply, divide } = require('./math')
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+console.log(add(2, 3));
+console.log(subtract(1, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 4));
+
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
 
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
-console.log(path.parse(__filename))
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+// console.log(path.parse(__filename))
 
