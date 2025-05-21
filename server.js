3e7ce43810f84d3748a121e4103cf904313d32d9
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-21 18:27:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const http = require('http');
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<h1>Hello, World!</h1>');
-    res.write('<h2>Hello There!</h2>');
-    res.end();
+    // res.write('<head><link rel="stylesheet" href="#"></head>');
+    // res.write('<h1>Hello, World!</h1>');
+    // res.write('<h2>Hello There!</h2>');
+    // res.end();
+
+    //send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
