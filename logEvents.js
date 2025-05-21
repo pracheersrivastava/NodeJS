86a3d6c37ec6856714151c3d6e410dfdd0d28568
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-21 16:40:52 +0530

    Add files via upload

---
+++ logEvents.js
@@ -0,0 +1,8 @@
+// console.log('testing!')
+
+const {format} = require('date-fns');
+const {v4 : uuid} = require('uuid');
+
+console.log(format(new Date(), 'yyyy-MM-dd\tHH:mm:ss'));
+
+console.log(uuid());
