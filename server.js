c8b9f7e8819b5459582e6e79e8c19721d9f923da
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-21 18:45:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,55 @@
-const http = require('http');
-const fs = require('fs');
+    const http = require('http');
+    const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    const server = http.createServer((req, res) => {
+        console.log(req.url, req.method);
 
-    // set header content type
-    res.setHeader('Content-Type', 'text/html');
-    // res.write('<head><link rel="stylesheet" href="#"></head>');
-    // res.write('<h1>Hello, World!</h1>');
-    // res.write('<h2>Hello There!</h2>');
-    // res.end();
+        let path = './views/';
+        switch (req.url) {
+            case '/':
+            case '/home':
+            case '/index':
+                path += 'index.html';
+                res.statusCode = 200;
+                break;
+            case '/about':
+                path += 'about.html';
+                res.statusCode = 200;
+                break;
+                //Redirect
+            case '/about-me':
+                res.statusCode = 301;
+                res.setHeader('Location', '/about');
+                res.end();           
+                return;
+            default:
+                path += '404.html';
+                res.statusCode = 404;
+                break;
 
-    //send an html file
-    fs.readFile('./views/index.html', (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end();
         }
-        else {
-            // res.write(data);
-            res.end(data);
-        }
+
+        // set header content type
+        res.setHeader('Content-Type', 'text/html');
+        // res.write('<head><link rel="stylesheet" href="#"></head>');
+        // res.write('<h1>Hello, World!</h1>');
+        // res.write('<h2>Hello There!</h2>');
+        // res.end();
+
+        //send an html file
+        fs.readFile(path, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.end();
+            }
+            else {
+                // res.write(data);
+                res.end(data);
+            }
+        });
     });
-});
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000');
-})
+    server.listen(3000, 'localhost', () => {
+        console.log('listening for requests on port 3000');
+    })
 
