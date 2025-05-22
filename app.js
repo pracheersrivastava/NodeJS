8045b1ab9f03b3bd08655e2d5aab2bacb8a1f68a
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2025-05-22 12:59:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ const { get } = require('lodash');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 //listen for requests
 app.listen(3000);
 app.get('/', (req, res) => {
     // res.send('<h1>Home Page</h1>');
-    res.sendFile('./views/index.html', { root: __dirname });
+        const blogs = [
+        {title: 'Blog 1', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+        {title: 'Blog 2', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+        {title: 'Blog 3', snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    ]
+
+    res.render('index', { title: 'Home', blogs });
 });
 app.get('/about', (req, res) => {
     // res.send('<h1>About Page</h1>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About'});
 });
 
-
-//redirect
-app.get('/about-me', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    // res.send('<h1>Create a new blog</h1>');
+    res.render('create', { title: 'Create'});
 });
 
-//404 page - use at the end as a catch all
+//404 page - use at the end as a catch all 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
-})
+    res.status(404).render('404', { title: '404'});
+})  
